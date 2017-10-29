18512d0858874a4a339a22daf2ac3fa1e6270028
imjavedkhan <javedalikhan50@gmail.com>
2017-10-29 16:36:18 +0530

    development on project

--- app/src/main/java/com/predator/medicon/SignupActivity.java
+++ app/src/main/java/com/predator/medicon/SignupActivity.java
@@ -1,13 +1,87 @@
 package com.predator.medicon;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-public class SignupActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignupActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText inputEmail, inputPassword;
+    private Button btnSignIn, btnSignUp, btnResetPassword;
+    private ProgressBar progressBar;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+
+        auth = FirebaseAuth.getInstance();
+
+        btnSignIn = (Button) findViewById(R.id.sign_in_button);
+        btnSignUp = (Button) findViewById(R.id.sign_up_button);
+        inputEmail = (EditText) findViewById(R.id.email);
+        inputPassword = (EditText) findViewById(R.id.password);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+
+        btnSignUp.setOnClickListener(this);
+        btnSignIn.setOnClickListener(this);
+
+    }
+
+    private void registerUser(){
+
+        String email = inputEmail.getText().toString().trim();
+        String password = inputPassword.getText().toString().trim();
+
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this,"Pls enter mail",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this,"Pls enter password",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        auth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+                            Toast.makeText(SignupActivity.this,"REGISTERED",Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Toast.makeText(SignupActivity.this,"TRY AGAIN",Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == btnSignUp) {
+            registerUser();
+        }
+        if(view == btnSignIn){
+            //sign in page
+        }
+
     }
 }
